8df507fdafacd1fc26cfe53225d13d384c71c3f9
Michael Sherman <msherman@pigging.com>
2022-01-07 16:57:24 -0600

    Add console support in app_threadx

--- Demo/Src/app_threadx.c
+++ Demo/Src/app_threadx.c
@@ -30,6 +30,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "usart.h"
+#include "console.h"
+#include "debug_menu_strings_en.h"
+#include "gpdma.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -53,6 +58,8 @@
 // Thread declarations
 //
 TX_THREAD txStartupTask;
+TX_THREAD txServicePortTask;
+
 
 /* USER CODE END PV */
 
@@ -62,6 +69,7 @@ TX_THREAD txStartupTask;
 //	Task function prototypes
 //
 void StartupTask_Entry(ULONG thread_input);
+void ServicePortTask_Entry(ULONG thread_input);
 
 //	Global byte pool point declaration
 TX_BYTE_POOL *pGlobal_byte_pool = TX_NULL;
@@ -111,6 +119,31 @@ UINT App_ThreadX_Init(VOID *memory_ptr)
     ret = TX_THREAD_ERROR;
   }
 
+  /* Allocate the stack for ServicePortTask.  */
+  if (tx_byte_allocate(pGlobal_byte_pool, (VOID **) &pMemPool,
+						 SERVICE_PORT_STACK_SIZE, TX_NO_WAIT) != TX_SUCCESS)
+  {
+
+	ret = TX_POOL_ERROR;
+
+  }
+
+  /* Create ServicePortTask.  */
+  if (tx_thread_create(	&txServicePortTask,
+												"Service Port Thread",
+												ServicePortTask_Entry,
+												0,
+												pMemPool,
+												SERVICE_PORT_STACK_SIZE,
+												THREAD_SERVICE_PORT_PRIO,
+												THREAD_SERVICE_PORT_PREEMPTION_THRESHOLD,
+												TX_NO_TIME_SLICE,
+												TX_AUTO_START) != TX_SUCCESS)
+
+  {
+	ret = TX_THREAD_ERROR;
+  }
+
 #endif
   /* USER CODE END App_ThreadX_Init */
 
@@ -136,7 +169,15 @@ void MX_ThreadX_Init(void)
 }
 
 /* USER CODE BEGIN 1 */
-
+cmd_t cmdlist[] =
+{
+  {"help",                                         "print this message",          help_cmd},
+  {"quit",                                                       "quit",          quit_cmd},
+  {"tile",                                                 "swarm tile",          tile_cmd},
+//  {"led",                                                    "led test",           led_cmd},
+//  {"gps",                                                    "gps info",           gps_cmd},
+  {  NULL,                                                         NULL,              NULL}
+};
 
 /* USER CODE BEGIN Header_StartStartupTask */
 /**
@@ -156,5 +197,40 @@ void StartupTask_Entry(ULONG thread_input)
 	tx_thread_sleep(500);
   }
 }
+
+/* USER CODE BEGIN Header_StartServicePortTask */
+/**
+  * @brief  Function implementing the servicePortTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartServicePortTask */
+void ServicePortTask_Entry(ULONG thread_input)
+{
+  UNUSED(thread_input);
+
+  /* Infinite loop */
+  while (1)
+  {
+#if INSTRUMENTATION
+		bSPTask = true;
+#endif
+
+		quit_msg = 0;
+		printf("\n\n\n\n\r#### Welcome to Qube #####\n");
+		printf("\n>>> Please choose one of the following commands:\n\n");
+		help_cmd(0, NULL);
+
+		/* Application body. */
+		console(USER_PROMPT, cmdlist);
+
+#if INSTRUMENTATION
+		bSPTask = false;
+#endif
+	/* Sleep for 500 ms */
+	tx_thread_sleep(500);
+  }
+}
+
 
 /* USER CODE END 1 */
